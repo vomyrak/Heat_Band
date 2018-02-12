4d02416de87e8aff274969b928d8bc23e08f2222
vomyrak <vomyrakm@gmail.com>
2018-02-12 12:07:37 +0000

    update onCreate function

--- app/src/main/java/com/example/vomyrak/heatband/MainActivity.java
+++ app/src/main/java/com/example/vomyrak/heatband/MainActivity.java
@@ -9,22 +9,93 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Create (3 preferences + 1 temp) * 3 bytes array for storing temperature info data
+    private byte[] stateVal = new byte[12];
+    //Create a byte for battery info
+    private byte batteryLife;
+
+    //Create UI elements
+    private ProgressBar progressBar;
+    private TextView tvBatteryLife;
+    private TextView tvTemperature;
+    private TextView tvMode1;
+    private TextView tvMode2;
+    private TextView tvMode3;
+    private CheckBox checkBox;
+    private Button applyChanges;
+    private Button select1;
+    private Button edit1;
+    private Button select2;
+    private Button edit2;
+    private Button select3;
+    private Button edit3;
+    private ToggleButton toggleButton;
+    private SeekBar seekBar;
+
+    //Create constant strings
+    private static final String mSettingStateVals = "stateVals";
+    private static final String mBatteryLife = "batteryLife";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null){
+            if (savedInstanceState.containsKey(mSettingStateVals)){
+                stateVal = savedInstanceState.getByteArray(mSettingStateVals);
+            }
+            if (savedInstanceState.containsKey(mBatteryLife)){
+                batteryLife = savedInstanceState.getByte(mBatteryLife);
+            }
+        }
         setContentView(R.layout.activity_main);
         SeekBar seekbar = (SeekBar) findViewById(R.id.temp_setter);
-        Button[] button = new Button[10];
-        for (int i = 0; i < 10; i++){
-            button[i] = (Button) findViewById(R.id.button + i);
-        }
+
 
     }
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putByteArray(mSettingStateVals, stateVal);
+        outState.putByte(mBatteryLife, batteryLife);
+    }
+
 
 
     @Override
