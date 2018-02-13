34023b0c06e52a0920987c05bee5bf90420fbda1
vomyrak <vomyrakm@gmail.com>
2018-02-13 12:32:17 +0000

    add floating activity

---
+++ app/src/main/java/com/example/vomyrak/heatband/FloatingActivity.java
@@ -0,0 +1,13 @@
+package com.example.vomyrak.heatband;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FloatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floating);
+    }
+}
--- app/src/main/java/com/example/vomyrak/heatband/MainActivity.java
+++ app/src/main/java/com/example/vomyrak/heatband/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.vomyrak.heatband;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.sax.StartElementListener;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +15,7 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -18,40 +23,53 @@ import android.widget.ToggleButton;
 
 import org.w3c.dom.Text;
 
+// TODO(1) Re-assignment of widget ids
+// TODO(2) Add a widget to display temperature
+
 public class MainActivity extends AppCompatActivity {
 
     //Create (3 preferences + 1 temp) * 3 bytes array for storing temperature info data
     private byte[] stateVal = new byte[12];
     //Create a byte for battery info
     private byte batteryLife;
-
+    //Create shared preference
+    protected SharedPreferences settings;
+    protected SharedPreferences.Editor editor;
     //Create UI elements
-    private ProgressBar progressBar;
-    private TextView tvBatteryLife;
-    private TextView tvTemperature;
-    private TextView tvMode1;
-    private TextView tvMode2;
-    private TextView tvMode3;
-    private CheckBox checkBox;
-    private Button applyChanges;
-    private Button select1;
-    private Button edit1;
-    private Button select2;
-    private Button edit2;
-    private Button select3;
-    private Button edit3;
-    private ToggleButton toggleButton;
-    private SeekBar seekBar;
+    protected ProgressBar progressBar;
+    protected TextView tvBatteryLife;
+    protected TextView tvTemperature;
+    protected TextView tvMode1;
+    protected TextView tvMode2;
+    protected TextView tvMode3;
+    protected ImageView imBluetooth;
+    protected Button applyChanges;
+    protected Button select1;
+    protected Button edit1;
+    protected Button select2;
+    protected Button edit2;
+    protected Button select3;
+    protected Button edit3;
+    protected ToggleButton toggleButton;
+    protected SeekBar seekBar;
 
     //Create constant strings
     private static final String mSettingStateVals = "stateVals";
     private static final String mBatteryLife = "batteryLife";
+    private static final String mPreferenceFile  = "MyPreferenceFile";
 
+    //Create bluetooth adaptor
+    private BluetoothAdapter bluetoothAdapter;
+
+    //A list of request codes
+    protected static final int rRequestBt = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Initialise app variables and UI elements on creation of application
         super.onCreate(savedInstanceState);
         if (savedInstanceState != null){
+            //If saved intance is present, get value from previously set instance state
             if (savedInstanceState.containsKey(mSettingStateVals)){
                 stateVal = savedInstanceState.getByteArray(mSettingStateVals);
             }
@@ -60,14 +78,44 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         setContentView(R.layout.activity_main);
-        SeekBar seekbar = (SeekBar) findViewById(R.id.temp_setter);
-
-
+        seekBar = (SeekBar) findViewById(R.id.temp_setter);
+        progressBar = (ProgressBar) findViewById(R.id.battery_life);
+        tvBatteryLife = (TextView) findViewById(R.id.battery);
+        tvTemperature = (TextView) findViewById(R.id.current_temp);
+        tvMode1 = (TextView) findViewById(R.id.mode_1);
+        tvMode2 = (TextView) findViewById(R.id.mode_2);
+        tvMode3 = (TextView) findViewById(R.id.mode_3);
+        toggleButton = (ToggleButton) findViewById(R.id.temp_unit);
+        imBluetooth = (ImageView) findViewById(R.id.bluetooth);
+        applyChanges = (Button) findViewById(R.id.change);
+        select1 = (Button) findViewById(R.id.select_1);
+        edit1 = (Button) findViewById(R.id.edit_1);
+        select2 = (Button) findViewById(R.id.select_2);
+        edit2 = (Button) findViewById(R.id.edit_2);
+        select3 = (Button) findViewById(R.id.select_3);
+        edit3 = (Button) findViewById(R.id.edit_3);
     }
+
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
+        //onStart, UI elements are associated with variables
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        //if (bluetoothAdapter == null) {
+            //bluetooth is not supported
+
+        //}
+        try {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                this.startActivityForResult(enableBtIntent, rRequestBt);
+            }
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            finish();
+        }
     }
+
 
     @Override
     protected void onResume(){
@@ -82,6 +130,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop(){
         super.onStop();
+        //Add shared preference settings
+        settings = getSharedPreferences(mPreferenceFile, 0);
+        editor = settings.edit();
+        editor.
+
     }
 
     @Override
