55139a1ad61ab8638d41342a59b5fa75783f7c15
vomyrak <vomyrakm@gmail.com>
2018-02-22 14:26:35 +0000

    bluetooth

--- app/src/main/java/com/example/vomyrak/heatband/FloatingActivity.java
+++ app/src/main/java/com/example/vomyrak/heatband/FloatingActivity.java
@@ -1,5 +1,6 @@
 package com.example.vomyrak.heatband;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,19 @@ public class FloatingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_floating);
+        Intent newIntent = getIntent();
+        int mode = newIntent.getIntExtra("Mode", 0);
+        if (mode != 0){
+            setTitle("Mode " + String.valueOf(mode));
+            byte data[] = new byte[3];
+            byte states[] = newIntent.getByteArrayExtra("stateVals");
+            int offset = mode * 3;
+            data[0] = states[offset];
+            data[1] = states[offset + 1];
+            data[2] = states[offset + 2];
+        }
+        else{finish();}
     }
 }
